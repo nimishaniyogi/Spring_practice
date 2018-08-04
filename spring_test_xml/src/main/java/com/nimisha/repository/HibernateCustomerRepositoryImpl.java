5ac849226ffea38ccb12c4d2d3bece33955ea8ac
nimishaniyogi <nimishaniyogi@gmail.com>
2018-08-04 15:36:09 -0500

    xml config properties

--- spring_test_xml/src/main/java/com/nimisha/repository/HibernateCustomerRepositoryImpl.java
+++ spring_test_xml/src/main/java/com/nimisha/repository/HibernateCustomerRepositoryImpl.java
@@ -3,17 +3,23 @@ package com.nimisha.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.nimisha.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+	@Value("${dbUsername}")
+	private String dbUsername;
 	
 	/* (non-Javadoc)
 	 * @see com.nimisha.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll () {
-List<Customer> customers = new ArrayList<>();
+		System.out.println(dbUsername);
+		
+		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
 		
