00c2a8c7125f913f73b9db229468aebdd14bdb07
nimishaniyogi <nimishaniyogi@gmail.com>
2018-08-04 14:50:47 -0500

    singleton java config

--- spring_test_java/src/main/java/com/nimisha/service/CustomerServiceImpl.java
+++ spring_test_java/src/main/java/com/nimisha/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.nimisha.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.nimisha.model.Customer;
@@ -10,6 +11,7 @@ import com.nimisha.repository.CustomerRepository;
 
 
 @Service("customerService")
+@Scope("prototype")
 public class CustomerServiceImpl implements CustomerService {
 
 private CustomerRepository customerRepository;
